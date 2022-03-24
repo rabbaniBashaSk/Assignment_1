6a16e3c0d43670404b940d3533331fc4b977ea21
rabbaniBashaSk <100215240+rabbaniBashaSk@users.noreply.github.com>
2022-03-24 09:55:01 +0530

    Update productActions.js

--- My-App/src/redux/actions/productActions.js
+++ My-App/src/redux/actions/productActions.js
@@ -2,29 +2,52 @@ import api from "../../api/products";
 import { ActionTypes } from "../constants/action-types";
 
 export const fetchProducts = () => async (dispatch) => {
-    const response = await api.get('/products');
+    try {
+        const response = await api.get('/products');
     dispatch({ type: ActionTypes.FETCH_PRODUCTS, payload: response.data })
+    } catch (e) {
+        alert('Something went wrong.');
+    }
 };
 
 export const fetchSelectedProduct = (params) => async (dispatch) => {
     const id = params.payload;
-    const response = await api.get(`/products/${id}`);
-    dispatch({ type: ActionTypes.SELECTED_PRODUCT, payload: response.data })
+    try {
+        const response = await api.get(`/products/${id}`);
+        dispatch({ type: ActionTypes.SELECTED_PRODUCT, payload: response.data })
+    } catch (e) {
+        alert('Something went wrong.');
+    }
 };
 
 export const removeSelectedProduct = (params) => async (dispatch) => {
     const id = params.payload;
-    await api.delete(`/products/${id}`);
-    dispatch(fetchProducts());
+    try {
+        await api.delete(`/products/${id}`);
+        dispatch(fetchProducts());
+        alert('Selected product deleted successfully.');
+    } catch (e) {
+        alert('Something went wrong.');
+    }    
 };
 
 export const editSelectedProduct = (params) => async (dispatch) => {
     const { id } = params.payload;
-    await api.put(`/products/${id}`, params.payload);
-    dispatch(fetchProducts());
+    try {
+        await api.put(`/products/${id}`, params.payload);
+        dispatch(fetchProducts());
+        alert('Selected product updated successfully.');
+    } catch (e) {
+        alert('Something went wrong.');
+    }
 };
 
 export const addNewProduct = (params) => async (dispatch) => {
-    await api.post(`/products`, params.payload);
-    dispatch(fetchProducts());
+    try {
+        await api.post(`/products`, params.payload);
+        dispatch(fetchProducts());
+        alert('New product added successfully.');
+    } catch (e) {
+        alert('Something went wrong.');
+    }
 };
